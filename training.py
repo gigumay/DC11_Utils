3b3dd346e199c480b793677dd9d1c9013ef62e13
gigumay <gigumay@gmail.com>
2024-02-28 10:49:27 +0100

    Added option for copying files instead of moving

--- training.py
+++ training.py
@@ -2,20 +2,24 @@ import shutil
 from pathlib import Path
 
 
-def retrieve_images(img_dir: str, ann_dir: str) -> int:
+def retrieve_images(img_dir: str, ann_dir: str, copy: bool) -> int:
     """
     For each annotation file in the annotation directory, retrieve the 
     corresponding image from the image directory.
     Arguments:
         img_dir (str):          path to the directory containing the images
         ann_dir (str):          path to the directory containing the annotation files
+        copy (bool):            if true, files are copied to the directory, else they are moved.
     Returns:
         the number of images retrieved
     """
     counter = 0
     for fn in Path(ann_dir).glob("*.txt"):
         img_fn = f"{img_dir}/{fn.stem}.jpg"
-        shutil.move(src=img_fn, dst=ann_dir)
+        if copy:
+            shutil.copy(src=img_fn, dst=ann_dir)
+        else: 
+            shutil.move(src=img_fn, dst=ann_dir)
         counter += 1
 
     return counter
@@ -39,17 +43,18 @@ def relocate_images(src_dir: str, dest_dir: str) -> int:
     return counter 
 
 
-def load_imgs(img_dir: str, ann_dir: str) -> int:
+def load_imgs(img_dir: str, ann_dir: str, copy: bool) -> int:
     """
     Executes 'retrieve_imgs()' for a given annotation directory and performs a few
     sanity checks to ensure that no files were lost. 
     Arguments:
         img_dir (str):          path to the directory containing the images
         ann_dir (str):          path to the directory containing the annotation files
+        copy (bool):            if true, files are copied to the directory, else they are moved.
     Returns:
         the number of images retrieved
     """
-    n_imgs = retrieve_images(img_dir=img_dir, ann_dir=ann_dir)
+    n_imgs = retrieve_images(img_dir=img_dir, ann_dir=ann_dir, copy=copy)
 
     assert n_imgs == len(list(Path((ann_dir)).glob("*.txt")))
     assert n_imgs == 0.5 * len(list(Path((ann_dir)).glob("*.*")))
@@ -76,28 +81,41 @@ def restore_dir(src_dir: str, dest_dir: str) -> int:
 
 
 
-def ready_yolo_dirs(img_dir: str, base_dir: str, mode: str, subdirs: list = ["train", "val", "test"]) -> list:
+def ready_yolo_dirs(img_dir: str, base_dir: str, copy: bool, subdirs: list = ["train", "val", "test"]) -> list:
     """
-    Ready directories containing annotation files for training. I.e., laod the corresponding images (if mode == 'retrieve'),
-    and return the images to their original directory. Probably not a very elegant solution.
+    Ready directories containing annotation files for training. I.e., load the corresponding images if mode == 'retrieve',
+    and return the images to their original directory  . Probably not a very elegant solution.
     Arguments:
         img_dir (str):          path to the images directory
         base_dir (str):         path to the directory containing the sub-directories used during yolo training
                                 (i.e., the training-, validation-, and test-directories)
-        mode (str):             whether to load the images into the respective directories, or put them back into 
-                                the original images directory
+        copy (bool):            if true, files are copied to the directory, not moved.
         subdirs (list):         names of the subdirectories
     Returns: 
-        A list containing the number of retrieved/relocated images for each subdirectory
+        A list containing the number of retrieved images for each subdirectory
     """
     img_counts = []
     for dir in subdirs:
-        if mode == "retrieve":
-            img_counts.append(retrieve_images(img_dir=img_dir, ann_dir=f"{base_dir}/{dir}"))
-        elif mode == "relocate":
-            img_counts.append(relocate_images(src_dir=f"{base_dir}/{dir}", dest_dir=img_dir)) 
-        else:
-            raise ValueError("Invalid mode passed to directory prep!")
+        img_counts.append(load_imgs(img_dir=img_dir, ann_dir=f"{base_dir}/{dir}"), copy=copy)
     
+    return img_counts
+
+
+
+def clean_yolo_dirs(img_dir: str, base_dir: str, subdirs: list = ["train", "val", "test"]) -> list:
+    """
+    Remove the image files from the yolo directories.
+    Arguments:
+        img_dir (str):          path to the images directory
+        base_dir (str):         path to the directory containing the sub-directories used during yolo training
+                                (i.e., the training-, validation-, and test-directories)
+        subdirs (list):         names of the subdirectories
+    Returns: 
+        A list containing the number of removed images for each subdirectory
+    """
+    img_counts = []
+    for dir in subdirs:
+        img_counts.append(restore_dir(src_dir=f"{base_dir}/{dir}", dest_dir=img_dir)) 
+
     return img_counts
 
